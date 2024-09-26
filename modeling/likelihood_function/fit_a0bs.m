75685015a52eea248c5bd3e5c99e97c7ff5a2a3f
jbchase <46462943+jbchase@users.noreply.github.com>
2024-09-26 13:51:11 -0700

    Add files via upload

---
+++ modeling/likelihood_function/fit_a0bs.m
@@ -0,0 +1,17 @@
+function [Xfit, ll, BIC, idx] = fit_a0bs(stimulus, action, reward, num_iter)
+
+obFunc = @(x) lik_a0bs(stimulus, action, reward, x(1), x(2), x(3));
+X_all = nan(num_iter, 3);
+nll_all = nan(num_iter, 1);
+LB = [0 0 -1];
+UB = [1 20 1];
+
+for i = 1:num_iter
+    X0 = [rand, rand * 20, rand * 2 - 1];
+    [X_all(i, :), nll_all(i)] = fmincon(obFunc, X0, [], [], [], [], LB, UB);
+end
+
+[~, idx] = min(nll_all);
+Xfit = X_all(idx, :);
+ll = -nll_all(idx);
+BIC = length(LB) * log(length(stimulus)) - 2 * ll;
